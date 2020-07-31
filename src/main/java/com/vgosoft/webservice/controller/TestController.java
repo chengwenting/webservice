ae1038b9e6d089b7570f64ba16290ccdd17f557e
chengwenting <577152369@qq.com>
2020-07-31 14:41:47 +0800

    测试

--- src/main/java/com/vgosoft/webservice/controller/TestController.java
+++ src/main/java/com/vgosoft/webservice/controller/TestController.java
@@ -1,11 +1,13 @@
 package com.vgosoft.webservice.controller;
 
+import org.springframework.stereotype.Controller;
+
 /**
  * @Description TODO
  * @Author Administrator
  * @Date 2020/7/31 14:05
  */
-@RestController
+@Controller
 public class TestController {
   
 }
