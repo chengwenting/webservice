312c95d40bde03ab2f67f3bcbf9e82ae81a94520
chengwenting <577152369@qq.com>
2020-07-31 14:06:22 +0800

    测试

---
+++ src/main/java/com/vgosoft/webservice/controller/TestController.java
@@ -0,0 +1,9 @@
+package com.vgosoft.webservice.controller;
+
+/**
+ * @Description TODO
+ * @Author Administrator
+ * @Date 2020/7/31 14:05
+ */
+public class TestController {
+}
--- src/test/java/com/vgosoft/webservice/WebserviceApplicationTests.java
+++ src/test/java/com/vgosoft/webservice/WebserviceApplicationTests.java
@@ -8,7 +8,7 @@ class WebserviceApplicationTests {
 
     @Test
     void contextLoads() {
-        system.out.println("测试");
+        System.out.println("测试");
     }
 
 }
